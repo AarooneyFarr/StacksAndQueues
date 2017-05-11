09d7464e886258839ba2f8416fc010c661cee873
AarooneyFarr <Aarooney.Farr@gmail.com>
2017-05-11 08:09:04 -0600

    added

--- src/stacks/controller/StackController.java
+++ src/stacks/controller/StackController.java
@@ -1,13 +1,30 @@
 package stacks.controller;
 
+import stacks.model.Customer;
+import java.util.LinkedList;
+import java.util.Stack;
+
+
 public class StackController
 	{
+		private LinkedList<Customer> customers;
+		private Stack<Customer> people;
+		
 		public StackController()
 		{
-			
+			customers = new LinkedList<Customer>();
+			people = new Stack<Customer>();
 		}
+		
 		public void start()
 		{
+			customers.add(new Customer());
+			customers.peek();
+			customers.remove();
+			
+			people.push(new Customer());
+			people.peek();
+			people.pop();
 			
 		}
 	}
